6620f48c0001844507ce5b584d4a705a1b34dafa
calvincxz <cxingzhu@hotmail.com>
2020-03-18 16:42:50 +0800

    complete backtracking for sudoku, with forward checking, and MRV, LCV heuristics

---
+++ CS3243_P2_Sudoku_40.py
@@ -0,0 +1,205 @@
+import sys
+import copy
+import math
+import time
+# Running script: given code can be run with the command:
+# python file.py, ./path/to/init_state.txt ./output/output.txt
+
+class Sudoku(object):
+    def __init__(self, puzzle):
+        # you may add more attributes if you need
+        self.puzzle = puzzle # self.puzzle is a list of lists
+        self.ans = copy.deepcopy(puzzle) # self.ans is a list of lists
+        self.domain = self.init_domain() # only called once during initialization
+        self.nodes = 0
+
+    def init_domain(self):
+        domain = {}
+        for i in range(81):
+            domain[i] = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        for row in range(9):
+            for col in range(9):
+                # store domain of variables with keys from 0 to 80.
+                index = row * 9 + col
+                value = puzzle[row][col]
+                if value != 0:
+                    domain = self.remove_values(row, col, value, domain) 
+        return domain
+
+    # Gets list of index for cells in same row, column and block for a given row, col.
+    def get_list_of_indices(self, row, col): 
+        indices = list()
+        for i in range(9):
+            if i != col:
+                indices.append(row * 9 + i)
+
+        for i in range(9):
+            if i != row:
+                indices.append(col + 9 * i)
+
+        block_row = math.floor(row / 3)
+        block_col = math.floor(col / 3)  
+        for i in range(3):
+            for j in range(3):
+                index = int((block_row * 3 + i) * 9 + block_col * 3 + j)
+                if index != row * 9 + col:
+                    indices.append(index)
+
+        return indices   
+
+    # Removes the specified value from constrained squares and returns the new dictionary
+    def remove_values(self, row, col, value, remaining_values):
+        remaining_values[row * 9 + col] = { value }
+
+        for index in self.get_list_of_indices(row, col):
+            remaining_values[index].discard(value)
+
+        return remaining_values
+
+    # return the list of empty squares
+    def get_empty_squares(self):
+        empty_squares = []
+        for row in range(9):
+            for col in range(9):
+                if puzzle[row][col] == 0:
+                    empty_squares.append([row, col]) 
+        return empty_squares
+
+    # returns the row, col of the most contrained empty square (MRV heuristic)
+    # returns None if all squares are filled
+    def get_most_constrained_square(self):
+        result = None
+        min_size = 10
+        for row in range(9):
+            for col in range(9):
+                # ignore non-empty cells
+                if puzzle[row][col] != 0:
+                    continue
+
+                index = row * 9 + col
+                size = len(self.domain[index])  
+
+                # optimize return since size of 1 is already the best result
+                if size == 1:
+                    return row, col
+
+                # find the square with domain of min. size
+                if size < min_size:
+                    min_size = size
+                    result = row, col
+
+        return result
+
+    def solve_backtrack(self):    
+        # MRV heuristic
+        most_constrainted_square = self.get_most_constrained_square()
+
+        if most_constrainted_square == None: 
+            return True
+        
+        row = most_constrainted_square[0]
+        col = most_constrainted_square[1]
+        values = self.domain[row * 9 + col]
+        indices = self.get_list_of_indices(row, col)
+        
+        while len(values) != 0: 
+            # select random variable
+            # value = values.pop()  
+
+            # select the least constraining value 
+            value = self.get_least_constraining_value(values, indices)
+            values.discard(value)
+            
+            # forward check for null domains    
+            if self.forward_check(value, indices):
+                puzzle[row][col] = value
+                temp = copy.deepcopy(self.domain)
+                self.domain = self.remove_values(row, col, value, self.domain)
+                self.nodes += 1
+
+                if self.solve_backtrack():
+                    return True
+                else:
+                    puzzle[row][col] = 0
+                    self.domain = temp
+                    
+        return False
+
+    # check cells in same row, col and block, returns the value which affects the least cells
+    # UNUSED HEURISTIC
+    def get_least_constraining_value(self, values, indices):
+        result = None
+        min_constraints = 1000
+        for value in values:
+            num_of_constraints = 0
+            for index in indices:
+                if value in self.domain[index]:
+                    num_of_constraints += 1
+
+            # update min
+            if min_constraints > num_of_constraints:
+                min_constraints = num_of_constraints
+                result = value
+
+        return result
+
+    # Checks if setting a value in a cell causes domain size of relevant cells to become 0
+    def forward_check(self, value, indices): 
+        for index in indices:
+            domain = self.domain[index]
+            if len(domain) == 1 and value in domain:
+                return False
+                                                 
+        return True
+    
+    def solve(self):
+        start = time.time()
+        if self.solve_backtrack():
+            print(self.puzzle)
+        else:
+            print("UNSOLVABLE")
+
+        end = time.time()
+        print("nodes generated: ", self.nodes)
+        print("duration: ", end - start)
+        # self.ans is a list of lists
+        return self.puzzle
+
+    # you may add more classes/functions if you think is useful
+    # However, ensure all the classes/functions are in this file ONLY
+    # Note that our evaluation scripts only call the solve method.
+    # Any other methods that you write should be used within the solve() method.
+
+if __name__ == "__main__":
+    # STRICTLY do NOT modify the code in the main function here
+    if len(sys.argv) != 3:
+        print ("\nUsage: python CS3243_P2_Sudoku_XX.py input.txt output.txt\n")
+        raise ValueError("Wrong number of arguments!")
+
+    try:
+        f = open(sys.argv[1], 'r')
+    except IOError:
+        print ("\nUsage: python CS3243_P2_Sudoku_XX.py input.txt output.txt\n")
+        raise IOError("Input file not found!")
+
+    puzzle = [[0 for i in range(9)] for j in range(9)]
+    lines = f.readlines()
+
+    i, j = 0, 0
+    for line in lines:
+        for number in line:
+            if '0' <= number <= '9':
+                puzzle[i][j] = int(number)
+                j += 1
+                if j == 9:
+                    i += 1
+                    j = 0
+
+    sudoku = Sudoku(puzzle)
+    ans = sudoku.solve()
+
+    with open(sys.argv[2], 'a') as f:
+        for i in range(9):
+            for j in range(9):
+                f.write(str(ans[i][j]) + " ")
+            f.write("\n")
